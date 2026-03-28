a2438c9ca82f94b10174ff21352585aa64579769
lukeslongo <luca.slongo@studenti.unipd.it>
2026-03-28 18:03:20 +0100

    fixato errore, mancava has_many in company model

--- backend/app/models/company.rb
+++ backend/app/models/company.rb
@@ -1,3 +1,6 @@
 class Company < ApplicationRecord
+  has_many :tones, dependent: :destroy
+  has_many :styles, dependent: :destroy
+  has_many :generated_data, dependent: :destroy
   validates :description, length: { maximum: 500, message: "La descrizione è troppo lunga (max 500 caratteri)" }
 end
