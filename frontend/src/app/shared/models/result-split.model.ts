06156626451bcc0e888de43010f87edf21d64bbf
Albero04 <albyp.2004@gmail.com>
2026-04-04 00:10:29 +0200

    levati comments

--- frontend/src/app/shared/models/result-split.model.ts
+++ frontend/src/app/shared/models/result-split.model.ts
@@ -1,6 +1,4 @@
 import {Result} from "./result.model";
-// nota1: ho aggiunto i campi che prima erano in ResultAiCopilot perchè li abbiamo spostati, come ha fatto il backend
-// nota2: ho aggiunto tutti i campit del recepient che restituisce il backend, questo perchè evitiamo così di fare chiamate per riceverli, se tanto alla fine dobbiamo mostrarli. Ho valutato attentamente ed è la soluzione più efficiente.
 export interface RecipientInfo {
     recipientId: number;
     recipientName: string;
